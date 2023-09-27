fc7d4db3d0c38fb5a826c6529cab3ff614330df1
abdullah ejaz <abdullahejaz@abdullahs-MacBook-Pro.local>
2023-09-27 13:19:05 +0500

    condition and logis

--- class4/Program.cs
+++ class4/Program.cs
@@ -4,23 +4,72 @@ namespace class4
 {
     internal class Program
     {
+
+        //Method to add 2 numbers
+        public static void Add(int a, int b)
+        {
+            int methodResult = a + b;
+            Console.WriteLine("Addition Result" + methodResult);
+        }
+        
+        public static void Subtract(int a, int b)
+        {
+            int methodResult = a - b;
+            Console.WriteLine("Subtraction Result" + methodResult);
+
+        }
+
+        public static void Divide(int a, int b)
+        {
+            int methodResult = a / b;
+            Console.WriteLine("Divide Result" + methodResult);
+
+        }
+        
+        
+        public static void Mulitply(int a, int b)
+        {
+            int methodResult = a * b;
+            Console.WriteLine("Multiply Result" + methodResult);
+
+        }
+        
+        public static void Calculate(int a,int b , string sign)
+        {
+
+            if (sign == "+")
+            {
+                Add(a,b);
+            }
+
+            if (sign == "-")
+            {
+                Subtract(a,b);
+            }
+
+            if (sign == "/")
+            {
+                Divide(a,b);
+            }
+
+            if (sign == "*")
+            {
+                Mulitply(a,b);
+            }
+        }
+        
+
+        
+        
+        
+
         public static void Main(string[] args)
         {
-            //Arthimetic Operators
-            int a = 4;
-            int b = 6;
-
-            int result = a + b;
-            Console.WriteLine(result);
-            
-            
-            // Comparison Operators
-            int r = 6;
-            int i = 10;
-            Console.WriteLine( a==b);
-
-            
-
+            Calculate(4,6,"*");
+            Calculate(7,3,"-");
+            Calculate(10,5,"/");
+            Calculate(5,5 ,"+");
+        
         }
     }
 }
