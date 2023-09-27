505d4fd8ecba01721b9c45bbb842901b170a78aa
abdullah ejaz <abdullahejaz@abdullahs-MacBook-Pro.local>
2023-09-27 11:46:47 +0500

    class 4 wrapper

--- class4/Program.cs
+++ class4/Program.cs
@@ -1,9 +1,26 @@
-﻿namespace class4
+﻿using System;
+
+namespace class4
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            //Arthimetic Operators
+            int a = 4;
+            int b = 6;
+
+            int result = a + b;
+            Console.WriteLine(result);
+            
+            
+            // Comparison Operators
+            int r = 6;
+            int i = 10;
+            Console.WriteLine( a==b);
+
+            
+
         }
     }
 }
